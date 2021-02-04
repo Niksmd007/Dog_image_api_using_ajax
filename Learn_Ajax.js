5a05a62532c020cd44ad62114e679f7f843fa742
Nikhil kumar <49806398+Niksmd007@users.noreply.github.com>
2021-02-05 00:01:28 +0530

    first commit

---
+++ Learn_Ajax.js
@@ -0,0 +1,14 @@
+function fetchRandomDogImage (){
+    var xhrRequest=new XMLHttpRequest();
+    xhrRequest.open('get','https://dog.ceo/api/breeds/image/random',true);
+    xhrRequest.send(); 
+    xhrRequest.onload=function()
+    {
+        console.log(xhrRequest.response);
+        var responseJSON=JSON.parse(xhrRequest.response);
+        var imageUrl=responseJSON.message;
+        $('#dog-image').attr('src',imageUrl);
+    }
+}
+
+$('#fetch-dog-image-button').click(fetchRandomDogImage);
